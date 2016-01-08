159df18513e2feb7fab81c7bfa45c47f286924a8
Matin <makhamzadeh@umail.ucsb.edu>
2016-01-07 17:43:27 -0800

    Added Date and Version

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,6 +1,15 @@
+/** MyFirst App is a sample program from p. 8-9 of head First java, 2nd edition.
+ @author Kathy Sierra and Bert Bates
+ @author Matin Akhamzadeh
+ @version 01/07/2016 for lab00, cs56, W16
+*/
+import java.util.Date;
+
 public class MyFirstApp{
     public static void main (String[] args){
 	System.out.println("I Rule!");
 	System.out.println("The World");
+	Date date = new Date();
+	System.out.println(date.toString());
     }
 }
